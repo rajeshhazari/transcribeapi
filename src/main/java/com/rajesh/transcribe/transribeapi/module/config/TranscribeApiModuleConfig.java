3986e9d5c8ed64ac14f6d0c775298b6d9bfad2c8
rajeshhazari <rajeshhazari@gmail.com>
2020-04-25 14:48:18 -0400

    added some junit tests

--- src/main/java/com/rajesh/transcribe/transribeapi/module/config/TranscribeApiModuleConfig.java
+++ src/main/java/com/rajesh/transcribe/transribeapi/module/config/TranscribeApiModuleConfig.java
@@ -132,6 +132,26 @@ public class TranscribeApiModuleConfig {
 		return mapper;
 	}
 	
+	/*
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}*/
+	
+	
 	private Connector redirectConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
